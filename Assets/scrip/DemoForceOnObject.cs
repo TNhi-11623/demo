3af60d852db1a9195761afd746a36c3eabdec6ac
T.Nhi <nguyenthinhi11623@gmail.com>
2025-06-14 15:37:17 +0700

    new script bodyphysics, demoForceOnObject

---
+++ Assets/scrip/DemoForceOnObject.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class DemoForceOnObject : MonoBehaviour
+{
+    [SerializeField] private Rigidbody targetRigidbody;
+    [SerializeField] private BodyPhysics bodyPhysics;
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Apply Unity Force") && targetRigidbody != null)
+        {
+            targetRigidbody.AddForce(transform.right * 10, ForceMode.Force);
+        }
+        if (GUILayout.Button("Apply Custom Force") && bodyPhysics != null)
+        {
+            bodyPhysics.AddForce(transform.right * 10);
+        }
+    }
+   
+}
