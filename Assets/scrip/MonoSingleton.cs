ffa7803ba8bd3a9266716beea6228021be9759d7
nhi nguyen <nguyenthinhi11623@gmail.com>
2025-07-04 14:22:19 +0700

    demo await and multithread, del GameManager

--- Assets/scrip/MonoSingleton.cs
+++ Assets/scrip/MonoSingleton.cs
@@ -1,23 +1,23 @@
 using UnityEngine;
+
 namespace Utils
 {
-    
-public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
-{
-    public static T Instance { get; private set; }
-    protected virtual void Awake()
+    public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
     {
-        if (Instance == null)
+        public static T Instance { get; private set; }
+
+        protected virtual void Awake()
         {
-            Instance = this as T;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Debug.LogWarning($"An instance of {GetType().Name} already exists. Destroying duplicate instance.");
+            if (Instance == null)
+            {
+                Instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Debug.LogWarning($"An instance of {GetType().Name} already exists. Destroying duplicate instance.");
                 Destroy(gameObject);
             }
         }
-}
-
+    }
 }
